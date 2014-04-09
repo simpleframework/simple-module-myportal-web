b2ccaec0b9412bf6779afbd70dce7ce26b632300
simpleframework <cknet@126.com>
2014-04-09 10:37:07 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/myportal/web/IMyPortalWebContext.java
+++ src/main/java/net/simpleframework/module/myportal/web/IMyPortalWebContext.java
@@ -1,6 +1,7 @@
 package net.simpleframework.module.myportal.web;
 
 import net.simpleframework.module.myportal.IMyPortalContext;
+import net.simpleframework.mvc.IMVCContextVar;
 import net.simpleframework.mvc.PageParameter;
 import net.simpleframework.mvc.common.element.AbstractElement;
 
@@ -10,7 +11,7 @@ import net.simpleframework.mvc.common.element.AbstractElement;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface IMyPortalWebContext extends IMyPortalContext {
+public interface IMyPortalWebContext extends IMyPortalContext, IMVCContextVar {
 
 	MyPortalUrlsFactory getUrlsFactory();
 
