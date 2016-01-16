c38c33ce3423508ef700204b370d400340ee3d5e
CHENK <CHENK@CHENK-PC>
2016-01-16 16:07:52 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/myportal/web/IMyPortalWebContext.java
+++ src/main/java/net/simpleframework/module/myportal/web/IMyPortalWebContext.java
@@ -1,7 +1,6 @@
 package net.simpleframework.module.myportal.web;
 
 import net.simpleframework.module.myportal.IMyPortalContext;
-import net.simpleframework.mvc.IMVCContextVar;
 import net.simpleframework.mvc.PageParameter;
 import net.simpleframework.mvc.common.element.AbstractElement;
 
@@ -11,7 +10,7 @@ import net.simpleframework.mvc.common.element.AbstractElement;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface IMyPortalWebContext extends IMyPortalContext, IMVCContextVar {
+public interface IMyPortalWebContext extends IMyPortalContext {
 
 	MyPortalUrlsFactory getUrlsFactory();
 
